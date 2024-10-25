3e29871d6f5ba048782aba163b60600effb91c10
logicallaw <logicallawbio@gmail.com>
2024-10-25 19:08:32 +0900

    add 3.23~4.18

--- 4.18.py
+++ 4.18.py
@@ -1,5 +1,42 @@
 import numpy as np
 
-A = np.array([[2,5,7],[6,2,3],[5,9,1]])
-tril = np.tril(A)
-print(tril)
+#1
+A = np.array([[1,1,2],[1,2,3],[1,1,3]])
+b = np.array([[-3],[-4],[-6]])
+invA = np.linalg.inv(A)
+
+x = np.matmul(invA, b)
+# print(x)
+
+#3
+A = np.array([[1,3,-1],[2,5,1],[1,1,1]])
+b = np.array([[1],[5],[3]])
+invA = np.linalg.inv(A)
+
+x = np.matmul(invA, b)
+# print(x)
+
+#4
+A = np.array([[2,1,1,-2],[3,-2,1,-6],[1,1,-1,-1],[5,-1,2,-8]])
+b = np.array([[1,-2,-1,3]])
+invA = np.linalg.inv(A)
+b = b.T
+
+x = np.matmul(invA, b)
+# print(x)
+
+#5
+A = np.array([[1,-3,4],[2,-5,7],[0,-1,2]])
+B = np.eye(3)
+invA = np.linalg.inv(A)
+
+x = np.matmul(invA, B)
+print(x)
+
+#6
+A = np.array([[1,1,2], [2,4,-3], [3,6,-5]])
+B = np.eye(3)
+invA = np.linalg.inv(A)
+
+x = np.matmul(invA, B)
+print(x)
